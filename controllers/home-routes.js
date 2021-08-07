acd6ed6c4d08b34db2eda830ca45f735501cd402
Nate Pfau <natepfau@yahoo.com>
2021-08-07 17:24:45 -0500

    Edit post page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -139,4 +139,51 @@ router.get('/post/:id', (req, res) =>
         });
 });
 
+router.get('/dashboard/edit/:id', withAuth, (req, res) =>
+{
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id',
+            'title',
+            'contents',
+            'createdAt',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'createdAt'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData =>
+        {
+            if (!dbPostData)
+            {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            const post = dbPostData.get({ plain: true });
+
+            res.render('edit-post', {
+                post,
+                loggedIn: true
+            });
+        })
+        .catch(err =>
+        {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
